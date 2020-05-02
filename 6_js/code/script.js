02862a076f4105497b6dfb75037d1304b7681b3c
neolectron <xeolectron@gmail.com>
2020-05-02 12:24:24 +0200

    #6 JS - introduction

---
+++ 6_js/code/script.js
@@ -0,0 +1,15 @@
+console.clear();
+
+console.log('bienvenue sur mon site');
+
+let pointDeVie = 100;
+let pointDeMana = 0;
+console.log('tes points de vies sont de : ');
+console.log(pointDeVie);
+
+pointDeVie = 50;
+pointDeVie = 10;
+
+console.log('BOOM HEADSHOT, t\'as plus que');
+console.log(pointDeVie);
+console.log('HP');
